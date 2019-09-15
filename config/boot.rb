265b04dd84c75b74ba3bcd8e42b0e1019bd3f50c
Alex Man <alex@hugebag.com>
2019-09-14 20:42:01 -0700

    ExecJS wasn't working with the JScript runtime on Windows properly so use Node.js as the runtime instead. See comment for more details

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,9 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+# ExecJS wasn't working properly with the JScript runtime on Windows so use
+# Node.js as the runtime instead. See the SO page below for details:
+# https://stackoverflow.com/questions/12520456/execjsruntimeerror-on-windows-trying-to-follow-rubytutorial/
+# Note that removing the "//= require_tree ." in application.js isn't a proper fix and modifying
+# ExecJS's runtimes.rb didn't fix the issue on Windows 10 64-bit
+ENV['EXECJS_RUNTIME'] = 'Node'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
